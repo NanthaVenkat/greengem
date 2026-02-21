63e3b6e13f8dd427979c07903aa8b210e4628a75
NnthaKumar <vnanthakumar00@gmail.com>
2026-02-21 09:18:16 +0530

    feat: Update layout styles for Header and Stages components for improved consistency and responsiveness

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 
 export default function Header() {
     return (
-        <header className="py-2 sm:py-4 px-4">
+        <header className="fixed top-0 left-0 z-10 py-2 sm:py-4 px-4 w-full">
             <div className="sm:max-w-[90%] mx-auto">
                 <Link to='/'><img src={logo} alt="Greengem" className="w-[120px] sm:w-[144px] mb-1 sm:mb-2" /></Link>
             </div>
